8391d4672e5cd403928e9af1bd2c410c2b4e4ddf
Omkar Sharma <omkarsharma2821@gmail.com>
2025-06-07 10:45:32 +0530

    awscicd

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Test Cloud Computing by Omkar Sharma</h1>
+      <h1>AWS CI/CD</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
