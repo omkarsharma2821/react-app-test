f2456db5e81d52b250744905b0d6cf7909cafa61
Omkar Sharma <omkarsharma2821@gmail.com>
2025-06-07 11:12:37 +0530

    omkar

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>AWS CI/CD</h1>
+      <h1>AWS CI/CD by omkar</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
